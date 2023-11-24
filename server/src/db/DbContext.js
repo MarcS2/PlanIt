da53855d50df7e93fecd85d96a38627ff3f92d2c
MarcS2 <marcellosharrai@hotmail.com>
2023-11-23 23:43:48 -0700

    fix

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
-import { ProjectSchema } from "../models/Project";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
