ef341beededa4476ce22d8c10025ad717aede0e0
MarcS2 <marcellosharrai@hotmail.com>
2023-11-25 10:54:32 -0700

    off-canvas added

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-1 ">
+        <section class="row ">
+          <div class="fs-2 text-white p-0 mt-3" style="width: 10dvh;"  data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            <p class="purple-bg text-center" role="button">P</p>
+
+          </div>
+
+        </section>
+      </div>
+      <div class="col-11">
+
+      </div>
+    </section>
+  </div>
+  <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
     </div>
   </div>
+  <div class="offcanvas-footer">
+    <div class="text-center">
+      <button class="btn btn-outline-primary mb-3 ">create project</button>
+
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -19,6 +43,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+.purple-bg {
+  background-color: #b129c3;
+}
+
+
 .home {
   display: grid;
   height: 80vh;
